f80f57ee55e80959c24ba442c2fc3c7a20bdd538
rpanday <rpanday@knoema.com>
2019-04-03 14:36:05 +0530

    fixed eslint warnings

--- webpack.config.js
+++ webpack.config.js
@@ -5,33 +5,33 @@ module.exports = {
     devtool: 'inline-source-map',
     entry: './src/index.js',
     output: {
-      path: path.resolve(__dirname, 'build'),
-      publicPath: '/',
-      filename: 'bundle.js'
+        path: path.resolve(__dirname, 'build'),
+        publicPath: '/',
+        filename: 'bundle.js'
     },
     devServer: {
-      contentBase: './build',
+        contentBase: './build',
     },
     module: {
-      rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader', 'eslint-loader']
-      },
-      {
-        test: /\.less$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'less-loader',
-        ],
-      },
-      ]
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader', 'eslint-loader']
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'less-loader',
+                ],
+            },
+        ]
     },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve('./index.html'),
-    }),
-  ]
-  };
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve('./index.html'),
+        }),
+    ]
+};
